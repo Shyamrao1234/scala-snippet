c2006a7d29b4cbc09e274d646ab934400fd759d4
Shyam <shyamrao.b@reactore.com>
2025-05-19 14:56:09 +0530

    WIP

--- src/main/scala/scalabasics/GetterNSetter.scala
+++ src/main/scala/scalabasics/GetterNSetter.scala
@@ -19,18 +19,61 @@ class Point {
 class Point1 {
   @BeanProperty var x: Int = 0
 }
-object Point1 extends App{
-  val p1=new Point1
+
+object Point1 extends App {
+  val p1 = new Point1
   p1.setX(20)
 
 }
 
 
-object GetterNSetter extends App{
+object GetterNSetter extends App {
 
-  val p=new Point
+  val p = new Point
   p.x = 20
   println(p.x)
+
+
+}
+
+
+object OverloadedMethod extends App {
+
+  //    def func(a: Int = 10) = {
+  //      a
+  //    }
+  //
+  //    def func(b: String = "B") = {
+  //      b
+  //    }
+  //
+  //    println(func())
+
+
+  //  def func(a: Int) = {
+  //    println("func A")
+  //    a + 10
+  //  }
+  //
+  //  def func(b: Int, x: String = "A") = {
+  //    println("func B")
+  //    b + 20
+  //  }
+  //
+  //  println(func(10))
+
+
+  def demo(a: => Int) = {
+    for (i <- 1 to 10) {
+      println(a)
+    }
+  }
+
+  var y = 0
+  demo {
+    y += 1
+    y
+  }
 
 
 }
