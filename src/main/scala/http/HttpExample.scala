42c13eb619957e205e7886fc32e708569b7103b0
shyam <shyamrao.b@reactore.com>
2025-09-25 09:52:25 +0530

    Scala basics

--- src/main/scala/http/HttpExample.scala
+++ src/main/scala/http/HttpExample.scala
@@ -1,6 +1,7 @@
 package http
 
-import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.model.HttpMethods
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 
 object HttpExample extends App {
@@ -10,4 +11,53 @@ object HttpExample extends App {
   val route1:Route=_.complete("yeah")
 
   val route2:Route = complete("yeah")
+
+  val a:Route={
+    println("MARK")
+    ctx=>ctx.complete("yeah")
+  }
+
+  val b:Route={ctx=>
+    println("MARK")
+    ctx.complete("yeah")
+  }
+
+  val route3:Route={ctx=>
+    if(ctx.request.method==HttpMethods.GET){
+      ctx.complete("recived get")
+    }else ctx.complete("recived something else")
+  }
+
+
+  val route3Simplify :Route=
+    concat(
+      get {
+        complete("yeah get recived")
+      },
+      complete("something else returned")
+    )
+
+  val route4 : Route = get{
+    complete("yeah")
+  }
+
+
+  val highLevelRoute=
+    path("order" / IntNumber){id=>
+      concat(
+        get{
+          complete("id:"+id)
+        },
+        put{
+          complete("id:"+id)
+        }
+      )
+    }
+
+  
+
+
+
+
+
 }
