92761ea8bc61204bb3913f1328314a64d981b34e
shyam <shyamrao.b@reactore.com>
2025-11-13 15:21:31 +0530

    WIP

---
+++ src/main/scala/scalabasics/ImplicitClassExamples.scala
@@ -0,0 +1,36 @@
+package scalabasics
+
+import org.json4s.JsonAST.JValue
+
+
+case class Person(name:String,age:Int)
+
+object ImplicitClassExamples extends App{
+
+  implicit class MyRichInt(number:Int) {
+   def isEven = number%2 == 0
+  }
+
+  println(2.isEven) // Internally it works like this new MyRichInt(2).isEven
+
+  //conversions
+  implicit  def string2person(name:String):Person = {
+    Person(name,24)
+  }
+
+  val shyam:Person="shyam"
+
+  println(shyam)
+
+  implicit class CustomJson(jValue: JValue) {
+
+
+
+    def asJson: String = {
+      ""
+    }
+  }
+
+
+
+}
